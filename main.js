4c6cd79e186e32212a9b8e5b38e9fa72136f5696
MaNnu81 <mannuspamm@gmail.com>
2025-02-25 12:03:11 +0100

    g

--- main.js
+++ main.js
@@ -1,84 +1,98 @@
 const fs = require("fs");
 
 
-function readCsvFromFile(filePath){
-    
-    const csv = fs.readFileSync( filePath, 'utf8');
+function readCsvFromFile(filePath) {
 
-    
+    const csv = fs.readFileSync(filePath, 'utf8');
 
     return csv;
+}
+
+function splitCsvInRows(csv){
+    const splittedLines = csv.split(/\r?\n/);
+    return splittedLines;
+}
 
 
-      
+function splitRows(arrayOfStringRows){
 
+    const splittedValue = [];
+
+    for (let i = 0; i < arrayOfStringRows.length; i++) {
+        const character = arrayOfStringRows[i];
+        const value = character.split(",");
+        splittedValue.push(value);
+        }
+        return splittedValue;
 }
 
 function fromCsvToJson(csv){
-    // `name,surname,yob,gender
-    //  lorenzo,puppo,1995,m
-    //  hugo,martinez,1994,m
-    //  sara,de prà,1989,f`
+   
     const arrayOfStringRows = splitCsvInRows(csv);
     // ["name,surname,yob,gender",
     //  "lorenzo,puppo,1995,m",
     //  "hugo,martinez,1994,m",
     //  "sara,de prà,1989,f"]
+    // console.log(arrayOfStringRows);
+    
     const arrayOfSplittedRows = splitRows(arrayOfStringRows);
-    // [["name","surname","yob","gender"],
-    // ["lorenzo","puppo","1995","m"],
-    // ["hugo","martinez","1994","m"],
-    // ["sara","de prà","1989","f"]]
-    const keys = getKeysFromFirsLine(arrayOfSplittedRows);
-    //["name","surname","yob","gender"]
-    const values = getValues(arrayOfSplittedRows);
-    // [["lorenzo","puppo","1995","m"],
-    // ["hugo","martinez","1994","m"],
-    // ["sara","de prà","1989","f"]]
-    const arrayFromEntries = createArrayOfEntries(keys, values);
-    // [
-    //     {
-    //         name: "lorenzo",
-    //         surname: "puppo",
-    //         yob: 1995,
-    //         gender: 'm'
-    //     },
-    //     {
-    //         name: "hugo",
-    //         surname: "martinez",
-    //         yob: 1994,
-    //         gender: 'm'
-    //     },
-    //     {
-    //         name: "sara",
-    //         surname: "de pra",
-    //         yob: 1989,
-    //         gender: 'f'
-    //     }
-    // ]
-    const json = convertArrayToJson(arrayFromEntries)
-    // '[
-    //     {
-    //         "name": "lorenzo",
-    //         "surname": "puppo",
-    //         "yob": 1995,
-    //         "gender": 'm'
-    //     },
-    //     {
-    //         "name": "hugo",
-    //         "surname": "martinez",
-    //         "yob": 1994,
-    //         "gender": 'm'
-    //     },
-    //     {
-    //         "name": "sara",
-    //         "surname": "de pra",
-    //         "yob": 1989,
-    //         "gender": 'f'
-    //     }
-    // ]'
+    
+    // // [["name","surname","yob","gender"],
+    // // ["lorenzo","puppo","1995","m"],
+    // // ["hugo","martinez","1994","m"],
+    // // ["sara","de prà","1989","f"]]
+    console.log(arrayOfSplittedRows);
 
-    return json
+    // const keys = getKeysFromFirsLine(arrayOfSplittedRows);
+    // //["name","surname","yob","gender"]
+    // const values = getValues(arrayOfSplittedRows);
+    // // [["lorenzo","puppo","1995","m"],
+    // // ["hugo","martinez","1994","m"],
+    // // ["sara","de prà","1989","f"]]
+    // const arrayFromEntries = createArrayOfEntries(keys, values);
+    // // [
+    // //     {
+    // //         name: "lorenzo",
+    // //         surname: "puppo",
+    // //         yob: 1995,
+    // //         gender: 'm'
+    // //     },
+    // //     {
+    // //         name: "hugo",
+    // //         surname: "martinez",
+    // //         yob: 1994,
+    // //         gender: 'm'
+    // //     },
+    // //     {
+    // //         name: "sara",
+    // //         surname: "de pra",
+    // //         yob: 1989,
+    // //         gender: 'f'
+    // //     }
+    // // ]
+    // const json = convertArrayToJson(arrayFromEntries)
+    // // '[
+    // //     {
+    // //         "name": "lorenzo",
+    // //         "surname": "puppo",
+    // //         "yob": 1995,
+    // //         "gender": 'm'
+    // //     },
+    // //     {
+    // //         "name": "hugo",
+    // //         "surname": "martinez",
+    // //         "yob": 1994,
+    // //         "gender": 'm'
+    // //     },
+    // //     {
+    // //         "name": "sara",
+    // //         "surname": "de pra",
+    // //         "yob": 1989,
+    // //         "gender": 'f'
+    // //     }
+    // // ]'
+
+    // return json
 }
 
 
@@ -86,11 +100,13 @@ function main(){
 
     const csvData = readCsvFromFile('./data/test1.csv');
 
-    console.log(csvData);
+    
 
-    // const json = fromCsvToJson(csvData);
+    const json = fromCsvToJson(csvData);
 
     // writeJsonToFile(filePath, json);
+    
+    
 }
 
 main()
