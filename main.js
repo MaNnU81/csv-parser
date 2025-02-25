1fc507e9a70afdae3acc385d0db03ea8c928e2ae
MaNnu81 <mannuspamm@gmail.com>
2025-02-25 10:48:42 +0100

    fd

--- main.js
+++ main.js
@@ -1,29 +1,96 @@
+const fs = require("fs");
 
-function readCsvFromFile(params) {
+
+function readCsvFromFile(filePath){
     
+    const csv = fs.readFileSync( filePath, 'utf8');
+
+    
+
+    return csv;
+
+
+      
+
 }
 
-function fromCsvToJson(csv) {
+function fromCsvToJson(csv){
+    // `name,surname,yob,gender
+    //  lorenzo,puppo,1995,m
+    //  hugo,martinez,1994,m
+    //  sara,de prà,1989,f`
     const arrayOfStringRows = splitCsvInRows(csv);
+    // ["name,surname,yob,gender",
+    //  "lorenzo,puppo,1995,m",
+    //  "hugo,martinez,1994,m",
+    //  "sara,de prà,1989,f"]
     const arrayOfSplittedRows = splitRows(arrayOfStringRows);
-    const keys = getKeysFromFirstLine(arrayOfSplittedRows);
+    // [["name","surname","yob","gender"],
+    // ["lorenzo","puppo","1995","m"],
+    // ["hugo","martinez","1994","m"],
+    // ["sara","de prà","1989","f"]]
+    const keys = getKeysFromFirsLine(arrayOfSplittedRows);
+    //["name","surname","yob","gender"]
     const values = getValues(arrayOfSplittedRows);
+    // [["lorenzo","puppo","1995","m"],
+    // ["hugo","martinez","1994","m"],
+    // ["sara","de prà","1989","f"]]
+    const arrayFromEntries = createArrayOfEntries(keys, values);
+    // [
+    //     {
+    //         name: "lorenzo",
+    //         surname: "puppo",
+    //         yob: 1995,
+    //         gender: 'm'
+    //     },
+    //     {
+    //         name: "hugo",
+    //         surname: "martinez",
+    //         yob: 1994,
+    //         gender: 'm'
+    //     },
+    //     {
+    //         name: "sara",
+    //         surname: "de pra",
+    //         yob: 1989,
+    //         gender: 'f'
+    //     }
+    // ]
+    const json = convertArrayToJson(arrayFromEntries)
+    // '[
+    //     {
+    //         "name": "lorenzo",
+    //         "surname": "puppo",
+    //         "yob": 1995,
+    //         "gender": 'm'
+    //     },
+    //     {
+    //         "name": "hugo",
+    //         "surname": "martinez",
+    //         "yob": 1994,
+    //         "gender": 'm'
+    //     },
+    //     {
+    //         "name": "sara",
+    //         "surname": "de pra",
+    //         "yob": 1989,
+    //         "gender": 'f'
+    //     }
+    // ]'
 
-    const arrayFromCsv = createArrayOfEntries(keys, value);
-    const json = convertArrayToJson
     return json
 }
 
 
+function main(){
 
-function main() {
-    
-const csvData = readCsvFromFile (filePath);
+    const csvData = readCsvFromFile('./data/test1.csv');
 
-const json = fromCsvToJson(csvData);
+    console.log(csvData);
 
-writeJsonToFile(filePath, json);
+    // const json = fromCsvToJson(csvData);
 
+    // writeJsonToFile(filePath, json);
 }
 
 main()
